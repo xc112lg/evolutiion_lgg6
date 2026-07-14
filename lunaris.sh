f550beb830dcb833a5df2b724cb77153e6a6e721
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-15 04:27:50 +0800

    Update lunaris.sh

--- lunaris.sh
+++ lunaris.sh
@@ -5,6 +5,7 @@
 
 rm -rf .repo/local_manifests/
 rm -rf device/lge
+rm -rf vendor/lge/msm8996-common
 #repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --depth=1 --git-lfs
 repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs --depth=1
 # repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs --depth=1
