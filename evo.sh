b17f8a71b2870dfe3bca76b250b614185c2f1558
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-15 19:29:08 +0800

    a

---
+++ evo.sh
@@ -0,0 +1,50 @@
+
+
+
+
+
+rm -rf .repo/local_manifests/
+rm -rf device/lge
+rm -rf vendor/lge/msm8996-common
+#repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --depth=1 --git-lfs
+#repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs --depth=1
+repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs --depth=1
+git clone https://github.com/xc112lg/local_manifests --depth 1 -b lg .repo/local_manifests
+repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
+/opt/crave/resync.sh
+# export TARGET_USES_PICO_GAPPS=true
+# export TARGET_ENABLE_BLUR=false
+# export WITH_ADB_INSECURE=true
+# export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GMS=false
+export TARGET_USES_PICO_GAPPS=true
+export CLANG_TARGET_ARM32="--target=arm-linux-android"
+
+source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/rbe8.sh)  >/dev/null 2>&1
+source build/envsetup.sh
+
+
+
+
+lunch lineage_h872-bp1a-userdebug
+#lunch lineage_h872-bp4a-userdebug
+make installclean
+m evolution
+
+# lunch lineage_h870-bp1a-userdebug
+
+# make installclean
+# m evolution -j64
+
+# lunch lineage_us997-bp1a-userdebug
+
+# make installclean
+# m evolution -j64
+
+#lunch lineage_h872-bp4a-eng
+#make installclean
+#make clean # one time
+#m bacon
+#m evolution
+
+curl -sf https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/upevo.sh  | bash >/dev/null 2>&1
