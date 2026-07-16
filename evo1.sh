28f7bf7d5b6eb7ce0c1e3597cfb540b28860d49d
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-16 16:35:49 +0800

    a

---
+++ evo1.sh
@@ -0,0 +1,31 @@
+export WITH_GMS=false
+export TARGET_USES_PICO_GAPPS=true
+
+source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/rbe8.sh)  >/dev/null 2>&1
+source build/envsetup.sh
+
+
+
+
+lunch lineage_h872-bp1a-userdebug
+#lunch lineage_h872-bp4a-userdebug
+make installclean
+m evolution
+
+lunch lineage_h870-bp1a-userdebug
+
+make installclean
+m evolution
+
+lunch lineage_us997-bp1a-userdebug
+
+make installclean
+m evolution
+
+#lunch lineage_h872-bp4a-eng
+#make installclean
+#make clean # one time
+#m bacon
+#m evolution
+
+curl -sf https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/upevo.sh  | bash >/dev/null 2>&1
