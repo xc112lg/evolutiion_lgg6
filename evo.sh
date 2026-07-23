37083ee55778f91e73c3c2584965fffb5ab3b032
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-23 08:18:22 +0800

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -56,7 +56,7 @@ source build/envsetup.sh
 
 # make installclean
 # m evolution
-
+sed -i 's/^SCO_WBS_SAMPLE_RATE = 0$/SCO_WBS_SAMPLE_RATE = 1/' device/lge/msm8996-common/bluetooth/vnd_lge_msm8996.txt
 lunch lineage_h872-bp1a-user
 #lunch lineage_h872-bp4a-userdebug
 make installclean
