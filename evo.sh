9b375459bab1494ae3d3ddd0d4c0e74ff83f7538
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-24 05:55:24 +0800

    s

--- evo.sh
+++ evo.sh
@@ -57,17 +57,17 @@ source build/envsetup.sh
 # make installclean
 # m evolution
 KBUSER=stendro_+_AShiningRay_+_continued_by_xc112lg
-KBHOST=crave.io
+KBHOST=crave
 
-export BUILD_USERNAME=stendro_+_AShiningRay_+_continued_by_xc112lg
-export BUILD_HOSTNAME=crave.io
+export BUILD_USERNAME=$KBUSER
+export BUILD_HOSTNAME=$KBHOST
 
 export KBUILD_BUILD_USER=$KBUSER
 export KBUILD_BUILD_HOST=$KBHOST
 
 
 echo "USER=$KBUILD_BUILD_USER HOST=$KBUILD_BUILD_HOST"
-
+echo "USER=$BUILD_USERNAME HOST=$BUILD_HOSTNAME"
 
 sed -i 's/^SCO_WBS_SAMPLE_RATE = 0$/SCO_WBS_SAMPLE_RATE = 1/' device/lge/msm8996-common/bluetooth/vnd_lge_msm8996.txt
 lunch lineage_h872-bp1a-user
