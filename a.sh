f1095efddab8c9cb6463661355f023ffce698f3c
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-24 16:01:53 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,1 +1,1 @@
-. <(curl -fsSL https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/evo.sh) axion
+. <(curl -fsSL https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/evo.sh)
