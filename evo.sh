2b2e93c6b6cbbe9792cb28bd5849002eba680f4f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-26 12:12:25 +0800

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -97,14 +97,14 @@ LINUX_COMPILE_HOST="crave.io"' \
 fi
 
 
-sed -i \
-  -e '/<path name="headphones-hifi-dac">/a\        <ctl name="Es9218 Bypass" value="0" />' \
-  -e '/<path name="headphones-hifi-dac-advanced">/a\        <ctl name="Es9218 Bypass" value="0" />' \
-  -e '/<path name="headphones-hifi-dac-aux">/a\        <ctl name="Es9218 Bypass" value="0" />' \
-  -e '/<path name="headphones-hifi-dacdop">/a\        <ctl name="Es9218 Bypass" value="0" />' \
-  -e '/<path name="headphones-hifi-dacdop-advanced">/a\        <ctl name="Es9218 Bypass" value="0" />' \
-  -e '/<path name="headphones-hifi-dacdop-aux">/a\        <ctl name="Es9218 Bypass" value="0" />' \
-  device/lge/g6-common/audio/mixer_paths_tasha.xml
+# sed -i \
+#   -e '/<path name="headphones-hifi-dac">/a\        <ctl name="Es9218 Bypass" value="0" />' \
+#   -e '/<path name="headphones-hifi-dac-advanced">/a\        <ctl name="Es9218 Bypass" value="0" />' \
+#   -e '/<path name="headphones-hifi-dac-aux">/a\        <ctl name="Es9218 Bypass" value="0" />' \
+#   -e '/<path name="headphones-hifi-dacdop">/a\        <ctl name="Es9218 Bypass" value="0" />' \
+#   -e '/<path name="headphones-hifi-dacdop-advanced">/a\        <ctl name="Es9218 Bypass" value="0" />' \
+#   -e '/<path name="headphones-hifi-dacdop-aux">/a\        <ctl name="Es9218 Bypass" value="0" />' \
+#   device/lge/g6-common/audio/mixer_paths_tasha.xml
 
 lunch lineage_h872-bp1a-user
 #lunch lineage_h872-bp4a-userdebug
