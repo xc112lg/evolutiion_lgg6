67753382df2fde1732bd695afb069769b5f48144
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-26 08:30:37 +0800

    Update and rename v1.sh to v.sh

--- v.sh
+++ v.sh
@@ -6,4 +6,5 @@ sed -i \
   -e '/<path name="headphones-hifi-dacdop-advanced">/a\        <ctl name="Es9218 Bypass" value="0" />' \
   -e '/<path name="headphones-hifi-dacdop-aux">/a\        <ctl name="Es9218 Bypass" value="0" />' \
   device/lge/g6-common/audio/mixer_paths_tasha.xml
-git diff device/lge/g6-common/audio/mixer_paths_tasha.xml
+cd device/lge/g6-common
+git diff
