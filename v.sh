6748b7a832ce2b0f0d0e1431399db1c477521d46
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-26 08:28:22 +0800

    Create v.sh

---
+++ v.sh
@@ -0,0 +1,9 @@
+sed -i \
+  -e '/<path name="headphones-hifi-dac">/a\        <ctl name="Es9218 Bypass" value="0" />' \
+  -e '/<path name="headphones-hifi-dac-advanced">/a\        <ctl name="Es9218 Bypass" value="0" />' \
+  -e '/<path name="headphones-hifi-dac-aux">/a\        <ctl name="Es9218 Bypass" value="0" />' \
+  -e '/<path name="headphones-hifi-dacdop">/a\        <ctl name="Es9218 Bypass" value="0" />' \
+  -e '/<path name="headphones-hifi-dacdop-advanced">/a\        <ctl name="Es9218 Bypass" value="0" />' \
+  -e '/<path name="headphones-hifi-dacdop-aux">/a\        <ctl name="Es9218 Bypass" value="0" />' \
+  device/lge/g6-common/audio/mixer_paths_tasha.xml
+  git diff device/lge/g6-common/audio/mixer_paths_tasha.xml
