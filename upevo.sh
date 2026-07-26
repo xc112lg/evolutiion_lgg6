134f24a9e2296891b5396243d9db3db0e12e12b3
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-26 14:21:48 +0800

    Clean up build script by removing unnecessary commands
    Removed redundant lunch and make commands for various devices.

--- upevo.sh
+++ upevo.sh
@@ -1,24 +1,3 @@
-source build/envsetup.sh
-
-
-lunch lineage_h870-bp1a-user
-make installclean
-m evolution
-
-lunch lineage_us997-bp1a-user
-make installclean
-m evolution
-
-lunch lineage_h873-bp1a-user
-#lunch lineage_h872-bp4a-userdebug
-make installclean
-m evolution
-
-lunch lineage_h870d-bp1a-user
-
-make installclean
-m evolution
-
 
 
 # Check and load environment variables from .env
