e7ed13eae960707c91e15292acad2476720244f0
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-27 08:50:50 +0800

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -107,7 +107,7 @@ sed -i \
   -e '/<path name="headphones-hifi-dacdop-aux">/a\        <ctl name="Es9218 Bypass" value="0" />' \
   device/lge/g6-common/audio/mixer_paths_tasha.xml
 export BUILD_BCR=false
-export TARGET_INCLUDE_VIPERFX=true
+#export TARGET_INCLUDE_VIPERFX=true
 export TARGET_ENABLE_BLUR=true
 
 
