cc3361c21c8d1a657d511109e70c070217fb512a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-01 03:52:33 +0800

    Update and rename evo1.sh to evo2.sh

--- evo2.sh
+++ evo2.sh
@@ -6,19 +6,9 @@
 export WITH_GMS=false
 export TARGET_USES_PICO_GAPPS=true
 
-source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/rbe8.sh)  >/dev/null 2>&1
+#source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/rbe8.sh)  >/dev/null 2>&1
 source build/envsetup.sh
 
-mkdir -p device/lge/msm8996-common/sepolicy/vendor-user
-if [ ! -f device/lge/msm8996-common/sepolicy/vendor-user/file.te ]; then
-    echo 'type sensors_data_file, file_type, data_file_type;' > device/lge/msm8996-common/sepolicy/vendor-user/file.te
-fi
-grep -q "sepolicy/vendor-user" device/lge/msm8996-common/BoardConfigCommon.mk || cat >> device/lge/msm8996-common/BoardConfigCommon.mk << 'EOF'
-
-ifeq ($(TARGET_BUILD_VARIANT),user)
-BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor-user
-endif
-EOF
 
 lunch lineage_h870-bp1a-user
 make installclean
