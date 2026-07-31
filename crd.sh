bbeebab2c5393ffc3c8a9d23e81e35f2b90fff16
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-31 13:11:46 +0800

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -47,6 +47,39 @@ source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/l
 #    sed -i 's|"maintainer": "\${MAINTAINER:-}"|"maintainer": "xc112lg"|' vendor/lineage/build/tools/createjson.sh
 #    sed -i 's|https://raw\.githubusercontent\.com/crdroidandroid|https://raw.githubusercontent.com/xc112lg|g' packages/apps/Settings/src/com/android/settings/deviceinfo/firmwareversion/BuildMaintainerPreference.kt
 
+mkdir -p device/lge/msm8996-common/sepolicy/private
+
+# priv_app.te
+[ -f device/lge/msm8996-common/sepolicy/private/priv_app.te ] || : > device/lge/msm8996-common/sepolicy/private/priv_app.te
+printf '%s\n' \
+  'allow priv_app mnt_pass_through_file:dir getattr;' \
+  'allow priv_app mnt_pass_through_file:dir search;' \
+  'allow priv_app qemu_hw_prop:file read;' \
+  'allow priv_app keyguard_config_prop:file read;' \
+  'allow priv_app storage_config_prop:file read;' \
+  'allow priv_app vendor_file:file read;' \
+  'allow priv_app vr_manager_service:service_manager find;' \
+  'allow priv_app ota_package_file:dir { read getattr };' \
+  >> device/lge/msm8996-common/sepolicy/private/priv_app.te
+
+# untrusted_app.te
+[ -f device/lge/msm8996-common/sepolicy/private/untrusted_app.te ] || : > device/lge/msm8996-common/sepolicy/private/untrusted_app.te
+printf '%s\n' \
+  'allow untrusted_app_32 vendor_file:file read;' \
+  'allow untrusted_app vendor_file:file read;' \
+  >> device/lge/msm8996-common/sepolicy/private/untrusted_app.te
+
+# vold_prepare_subdirs.te
+[ -f device/lge/msm8996-common/sepolicy/private/vold_prepare_subdirs.te ] || : > device/lge/msm8996-common/sepolicy/private/vold_prepare_subdirs.te
+printf '%s\n' \
+  'allow vold_prepare_subdirs vendor_file:file read;' \
+  >> device/lge/msm8996-common/sepolicy/private/vold_prepare_subdirs.te
+
+# recheck
+echo "== priv_app.te =="; cat device/lge/msm8996-common/sepolicy/private/priv_app.te
+echo "== untrusted_app.te =="; cat device/lge/msm8996-common/sepolicy/private/untrusted_app.te
+echo "== vold_prepare_subdirs.te =="; cat device/lge/msm8996-common/sepolicy/private/vold_prepare_subdirs.te
+
 
 
 source build/envsetup.sh
