85ad4430cad49ea64b8f0210e99243fbe77d2ce9
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-31 16:12:21 +0800

    Update rbe8.sh

--- rbe8.sh
+++ rbe8.sh
@@ -20,8 +20,8 @@ else
     echo "[PATCH] Already applied (or conflicts), skipping."
 fi
 
-export RBE_service="xc112lg.buildbuddy.io:443"        # BuildBuddy instance address (without grpcs://, add the port 443)
-export RBE_remote_headers="x-buildbuddy-api-key=D2SvmJdB1v8oM6KaNg6J"    # Your BuildBuddy API key
+export RBE_service="xc2.buildbuddy.io:443"        # BuildBuddy instance address (without grpcs://, add the port 443)
+export RBE_remote_headers="x-buildbuddy-api-key=y5iqIkFp4iRpu1p6XaqX"    # Your BuildBuddy API key
 export RBE_use_rpc_credentials=false
 export RBE_service_no_auth=false
 
