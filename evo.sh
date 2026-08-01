ebef48304c552505e0393e8fae5efda2dd32e063
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-01 10:36:24 +0800

    Clean up evo.sh and enhance background blur support
    Removed commented-out code and unnecessary commands related to ViPER4Android and sepolicy. Added support for background blur and updated configuration files.

--- evo.sh
+++ evo.sh
@@ -23,81 +23,7 @@ export WITH_GMS=false
 export TARGET_USES_PICO_GAPPS=true
 sed -i '$a -include vendor/evolution-priv/keys/keys.mk' device/lge/msm8996-common/msm8996.mk
 
-mkdir -p device/lge/msm8996-common/sepolicy/vendor-user
-if [ ! -f device/lge/msm8996-common/sepolicy/vendor-user/file.te ]; then
-    echo 'type sensors_data_file, file_type, data_file_type;' > device/lge/msm8996-common/sepolicy/vendor-user/file.te
-fi
-grep -q "sepolicy/vendor-user" device/lge/msm8996-common/BoardConfigCommon.mk || cat >> device/lge/msm8996-common/BoardConfigCommon.mk << 'EOF'
-
-ifeq ($(TARGET_BUILD_VARIANT),user)
-BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor-user
-endif
-EOF
-
-
-#source <(curl -sf https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/rbe8.sh)  >/dev/null 2>&1
-
- mkdir -p device/lge/msm8996-common/overlay/frameworks/base/core/res/res/values
-    curl -sf -o device/lge/msm8996-common/overlay/frameworks/base/core/res/res/values/cr_config.xml \
-        https://raw.githubusercontent.com/crdroidandroid/android_frameworks_base/15.0/core/res/res/values/cr_config.xml
-
-    mkdir -p device/lge/msm8996-common/overlay/frameworks/base/packages/SystemUI/res/values
-    curl -sf -o device/lge/msm8996-common/overlay/frameworks/base/packages/SystemUI/res/values/cr_config.xml \
-        https://raw.githubusercontent.com/crdroidandroid/android_frameworks_base/15.0/packages/SystemUI/res/values/cr_config.xml
-#    sed -i 's|"maintainer": "\${MAINTAINER:-}"|"maintainer": "xc112lg"|' vendor/lineage/build/tools/createjson.sh
-#    sed -i 's|https://raw\.githubusercontent\.com/crdroidandroid|https://raw.githubusercontent.com/xc112lg|g' packages/apps/Settings/src/com/android/settings/deviceinfo/firmwareversion/BuildMaintainerPreference.kt
-
-
-
 source build/envsetup.sh
-
-
-
-
-# lunch lineage_h872-bp1a-userdebug
-# #lunch lineage_h872-bp4a-userdebug
-# make installclean
-# m evolution
-
-# lunch lineage_h870-bp1a-userdebug
-
-# make installclean
-# m evolution
-
-# lunch lineage_us997-bp1a-userdebug
-
-# make installclean
-# m evolution
-
-# rm -rf packages/apps/ViPER4AndroidFX
-# git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
-
-# # 1. msm8996.mk — inherit the V4A product config
-# sed -i '/\$(call inherit-product, vendor\/lge\/msm8996-common\/msm8996-common-vendor.mk)/a\
-# \
-# # ViPER4Android\
-# $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)' device/lge/msm8996-common/msm8996.mk
-
-# # 2. audio/audio_effects.xml — register the v4a_re library
-# sed -i '/<library name="volume_listener" path="libvolumelistener.so"\/>/a\
-#         <library name="v4a_re" path="libv4a_re.so"/>' device/lge/msm8996-common/audio/audio_effects.xml
-
-# # 3. audio/audio_effects.xml — register the v4a_standard_re effect
-# sed -i '/<effect name="notification_helper" library="volume_listener" uuid="0b776dde-0590-11e5-81ba-0025b32654a0"\/>/a\
-#         <effect name="v4a_standard_re" library="v4a_re" uuid="90380da3-8536-4744-a6a3-5731970e640f"/>' device/lge/msm8996-common/audio/audio_effects.xml
-
-# # 4. sepolicy/vendor/audioserver.te — file access + prop read
-# sed -i '/allow audioserver mpctl_socket:sock_file write;/a\
-# \
-# # ViPER4Android\
-# get_prop(audioserver, vendor_audio_prop)\
-# allow audioserver unlabeled:file { read write open getattr };' device/lge/msm8996-common/sepolicy/vendor/audioserver.te
-
-# # 5. sepolicy/vendor/hal_audio_default.te — execmem for the native effect
-# sed -i '/allow hal_audio_default init:unix_stream_socket connectto;/a\
-# \
-# # ViPER4Android\
-# allow hal_audio_default hal_audio_default:process { execmem };' device/lge/msm8996-common/sepolicy/vendor/hal_audio_default.te
 sed -i '/ro.hardware.egl=adreno \\/a\    ro.surface_flinger.supports_background_blur=1 \\' device/lge/msm8996-common/vendor_prop.mk
 #cat device/lge/msm8996-common/vendor_prop.mk
 KERNEL_DIR="kernel/lge/msm8996"
@@ -109,8 +35,6 @@ LINUX_COMPILE_BY="stendro_+_AShiningRay_+_continued_by_xc112lg"' \
 LINUX_COMPILE_HOST="crave.io"' \
     "$KERNEL_DIR/scripts/mkcompile_h"
 fi
-
-
 sed -i \
   -e '/<path name="headphones-hifi-dac">/a\        <ctl name="Es9218 Bypass" value="0" />' \
   -e '/<path name="headphones-hifi-dac-advanced">/a\        <ctl name="Es9218 Bypass" value="0" />' \
@@ -122,8 +46,6 @@ sed -i \
 export BUILD_BCR=false
 #export TARGET_INCLUDE_VIPERFX=true
 export TARGET_ENABLE_BLUR=true
-
-
 
 sed -i '/<\/resources>/i\
 \    <!-- Blur radius behind Notification Shade -->\n    <dimen name="max_shade_window_blur_radius">17dp</dimen>\n' "device/lge/msm8996-common/overlay/frameworks/base/packages/SystemUI/res/values/config.xml"
@@ -180,7 +102,6 @@ EOF
 
 # Register in msm8996.mk
 sed -i '/^    WifiOverlay \\$/a\    LauncherOverlayMsm8996 \\' device/lge/msm8996-common/msm8996.mk
-
 lunch lineage_h872-bp1a-userdebug
 #lunch lineage_h872-bp4a-userdebug
 # breakfast h872
