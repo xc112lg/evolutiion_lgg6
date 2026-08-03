0c570754022a046d2f451d28f287864d3667d82d
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-03 19:03:04 +0800

    ss

--- crd1.sh
+++ crd1.sh
@@ -50,43 +50,12 @@ export BUILD_BCR=false
 export TARGET_ENABLE_BLUR=true
 #export WITH_ADB_INSECURE=true
 
-grep -q '^[[:space:]]*# props\.append("ro\.adb\.secure=1")' build/soong/scripts/gen_build_prop.py ||
-sed -i 's/^\([[:space:]]*\)props\.append("ro\.adb\.secure=1")/\1# props.append("ro.adb.secure=1")/' build/soong/scripts/gen_build_prop.py
-
-mkdir -p device/lge/msm8996-common/sepolicy/private
-
-# priv_app.te
-[ -f device/lge/msm8996-common/sepolicy/private/priv_app.te ] || : > device/lge/msm8996-common/sepolicy/private/priv_app.te
-printf '%s\n' \
-  'allow priv_app mnt_pass_through_file:dir getattr;' \
-  'allow priv_app mnt_pass_through_file:dir search;' \
-  'allow priv_app qemu_hw_prop:file read;' \
-  'allow priv_app keyguard_config_prop:file read;' \
-  'allow priv_app storage_config_prop:file read;' \
-  'allow priv_app vendor_file:file read;' \
-  'allow priv_app vr_manager_service:service_manager find;' \
-  'allow priv_app ota_package_file:dir { read getattr };' \
-  >> device/lge/msm8996-common/sepolicy/private/priv_app.te
-
-# untrusted_app.te
-[ -f device/lge/msm8996-common/sepolicy/private/untrusted_app.te ] || : > device/lge/msm8996-common/sepolicy/private/untrusted_app.te
-printf '%s\n' \
-  'allow untrusted_app_32 vendor_file:file read;' \
-  'allow untrusted_app vendor_file:file read;' \
-  >> device/lge/msm8996-common/sepolicy/private/untrusted_app.te
-
-# vold_prepare_subdirs.te
-[ -f device/lge/msm8996-common/sepolicy/private/vold_prepare_subdirs.te ] || : > device/lge/msm8996-common/sepolicy/private/vold_prepare_subdirs.te
-printf '%s\n' \
-  'allow vold_prepare_subdirs vendor_file:file read;' \
-  >> device/lge/msm8996-common/sepolicy/private/vold_prepare_subdirs.te
+# grep -q '^[[:space:]]*# props\.append("ro\.adb\.secure=1")' build/soong/scripts/gen_build_prop.py ||
+# sed -i 's/^\([[:space:]]*\)props\.append("ro\.adb\.secure=1")/\1# props.append("ro.adb.secure=1")/' build/soong/scripts/gen_build_prop.py
 
 
 #sed -i 's/^BOARD_SEPOLICY_DIRS += \$(DEVICE_COMMON_PATH)\/sepolicy$/BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_COMMON_PATH)\/sepolicy/' device/lge/g6-common/BoardConfigCommon.mk
 
-
-# grep -q '^[[:space:]]*# props\.append("ro\.adb\.secure=1")' build/soong/scripts/gen_build_prop.py || sed -i '353s/^\([[:space:]]*\)props\.append("ro\.adb\.secure=1")/\1# props.append("ro.adb.secure=1")/' build/soong/scripts/gen_build_prop.py
-# grep -n -A2 -B2 'ro.adb.secure' build/soong/scripts/gen_build_prop.py
 
 sed -i '$r /dev/stdin' device/lge/msm8996-common/sepolicy/vendor/file_contexts <<'EOF'
 
