aba3b1d2767816606a08b19441c1a5a816be6376
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-04 06:00:43 +0800

    s

--- crd1.sh
+++ crd1.sh
@@ -2,7 +2,7 @@ rm -rf .repo/local_manifests/
 rm -rf device/lge
 rm -rf vendor/lge/msm8996-common kernel/lge/msm8996
 rm -rf vendor/evolution-priv/keys vendor/lineage-priv/keys
-rm -rf build/soong
+#rm -rf build/soong
 #rm -rf out/target/product/*/obj/KERNEL_OBJ
 
 #repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --depth=1 --git-lfs
@@ -33,17 +33,7 @@ sed -i '$a -include vendor/lineage-priv/keys/keys.mk' device/lge/msm8996-common/
 source build/envsetup.sh
 
 
-sed -i '/ro.hardware.egl=adreno \\/a\    ro.surface_flinger.supports_background_blur=1 \\' device/lge/msm8996-common/vendor_prop.mk
-#cat device/lge/msm8996-common/vendor_prop.mk
-KERNEL_DIR="kernel/lge/msm8996"
-if ! grep -q "stendro_+_AShiningRay_+_continued_by_xc112lg" "$KERNEL_DIR/scripts/mkcompile_h"; then
-  sed -i \
-    -e '/if test -z "\$KBUILD_BUILD_USER"; then/,/^fi$/c\
-LINUX_COMPILE_BY="stendro_+_AShiningRay_+_continued_by_xc112lg"' \
-    -e '/if test -z "\$KBUILD_BUILD_HOST"; then/,/^fi$/c\
-LINUX_COMPILE_HOST="crave.io"' \
-    "$KERNEL_DIR/scripts/mkcompile_h"
-fi
+
 
 export BUILD_BCR=false
 #export TARGET_INCLUDE_VIPERFX=true
@@ -57,31 +47,13 @@ export TARGET_ENABLE_BLUR=true
 #sed -i 's/^BOARD_SEPOLICY_DIRS += \$(DEVICE_COMMON_PATH)\/sepolicy$/BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_COMMON_PATH)\/sepolicy/' device/lge/g6-common/BoardConfigCommon.mk
 
 
-sed -i '$r /dev/stdin' device/lge/msm8996-common/sepolicy/vendor/file_contexts <<'EOF'
 
-# ODM sepolicy fragments
-# AOSP's private/file_contexts only matches /(odm|vendor/odm)/etc/selinux/...
-# On this device there is no separate /vendor partition (merged into
-# /system/vendor), so the real runtime path is /system/vendor/odm/etc/selinux/...
-# which falls outside that pattern and was falling back to the generic
-# vendor_file label, causing zygote/installd/system_server (and adb shell)
-# to be denied getattr/read on these files. Types below match AOSP's own
-# per-file types (see system/sepolicy private/file_contexts) exactly.
-/system/vendor/odm/etc/selinux/odm_file_contexts                 u:object_r:file_contexts_file:s0
-/system/vendor/odm/etc/selinux/odm_seapp_contexts                u:object_r:seapp_contexts_file:s0
-/system/vendor/odm/etc/selinux/odm_property_contexts             u:object_r:property_contexts_file:s0
-/system/vendor/odm/etc/selinux/odm_service_contexts              u:object_r:vendor_service_contexts_file:s0
-/system/vendor/odm/etc/selinux/odm_hwservice_contexts            u:object_r:hwservice_contexts_file:s0
-/system/vendor/odm/etc/selinux/odm_mac_permissions\.xml          u:object_r:mac_perms_file:s0
-/system/vendor/odm/etc/selinux/odm_sepolicy\.cil                 u:object_r:sepolicy_file:s0
-EOF
-
-cat device/lge/msm8996-common/sepolicy/vendor/file_contexts
 
 
 
 curl -sL https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/init.qcom.usb.rc.patch | patch -d device/lge/msm8996-common -p0
-
+source <(curl -sf https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/blur.sh)
+source <(curl -sf https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/sepolicycrdfix.sh)
 lunch lineage_h872-bp1a-userdebug
 
 make installclean
