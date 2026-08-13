f2bd659870d38eadad6ad98e54ad597d7f3ce948
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-14 06:09:22 +0800

    Update evotest.sh

--- evotest.sh
+++ evotest.sh
@@ -80,4 +80,4 @@ m evolution
 #m bacon
 #m evolution
 
-curl -sf https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/upevo.sh  | bash >/dev/null 2>&1
+curl -sf https://raw.githubusercontent.com/xc112lg/testonly/refs/heads/main/testevo.sh  | bash >/dev/null 2>&1
