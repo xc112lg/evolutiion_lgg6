54bb33e0f3e8d7df821b5ae0ce6f914e028ac047
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-21 20:38:23 +0800

    Update and rename aa.sh to aaaa.sh

--- aaaa.sh
+++ aaaa.sh
@@ -1,9 +1,9 @@
 # 1. Clone your repo
-git clone https://github.com/xc112lg/android_kernel_lge_msm8996_r2.git
-cd android_kernel_lge_msm8996_r2
+git clone https://github.com/LineageOS/android_kernel_lge_msm8996.git
+cd android_kernel_lge_msm8996
 
 # 2. Check out your target branch
-git checkout swan26
+git checkout lineage-22.1
 
 # 3. Add Inkypen79's msm8998 repo as a remote and fetch it
 git remote add msm8998 https://github.com/Inkypen79/android_kernel_qcom_msm8998.git
