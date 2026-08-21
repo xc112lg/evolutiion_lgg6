cb0c8b7ad1d6d6ae561fe2101a4e5863cd24b886
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-21 21:08:38 +0800

    Update xx.sh

--- xx.sh
+++ xx.sh
@@ -6,11 +6,11 @@ cd android_kernel_lge_msm8996_r2
 git checkout swan26
 
 # 3. Add Inkypen79's msm8998 repo as a remote and fetch it
-git remote add msm8996 https://github.com/Inkypen79/android_kernel_lge_msm8996.git
-git fetch msm8996
+git remote add msm8997 https://github.com/Inkypen79/android_kernel_lge_msm8996.git
+git fetch msm8997
 
 # 4. Merge lineage-24.0 from that remote into swan24
-git merge -X theirs msm8996/lineage-24.0 --allow-unrelated-histories -m "Merge https://github.com/Inkypen79/android_kernel_qcom_msm8998 into swan24"
+git merge -X theirs msm8997/lineage-24.0 --allow-unrelated-histories -m "Merge https://github.com/Inkypen79/android_kernel_qcom_msm8998 into swan24"
 
 # 5. Resolve any conflicts if they come up
 git status
