7110cdb4b87ea10e1e3baa5697c406bdc0262402
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-27 21:09:58 +0800

    sse

--- b.sh
+++ b.sh
@@ -1,1 +1,1 @@
-. <(curl -fsSL https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/crd.sh)
+curl -sf https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/crd16eng.sh | bash
