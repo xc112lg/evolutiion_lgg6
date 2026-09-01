4ba79f1d731ca71625c4a0170f37d5b0114716c4
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-09-01 09:16:04 +0800

    s

--- b1.sh
+++ b1.sh
@@ -1,1 +1,1 @@
-. <(curl -fsSL https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/evo5.sh)
+curl -sf https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/a17.sh | bash
