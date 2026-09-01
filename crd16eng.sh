df8a3547c5935d4eeafa6934239de9ad6e083029
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-09-01 08:08:44 +0800

    Update crd16eng.sh

--- crd16eng.sh
+++ crd16eng.sh
@@ -86,7 +86,8 @@ for MOD in \
     "libmmjpeg_interface" \
     "libqomx_core" \
     "libmmcamera_tuning" \
-    "libmm-qcamera"; do
+    "libmm-qcamera"
+    mm-qcamera-app; do
 
     perl -0777 -i -pe "
     while (/cc_prebuilt_library_shared\s*\{/g) {
@@ -139,6 +140,9 @@ sed -i \
     -e '/^[[:space:]]*libmmcamera_tuning[[:space:]]*\\\{0,1\}[[:space:]]*$/d' \
     -e '/^[[:space:]]*mm-qcamera-app[[:space:]]*\\\{0,1\}[[:space:]]*$/d' \
     vendor/lge/g6-common/g6-common-vendor.mk
+
+for MOD in "camera\.msm8996" "libmmcamera_interface" "libmmjpeg_interface" "libqomx_core" "libmmcamera_tuning" "libmm-qcamera" "mm-qcamera-app"; do perl -i -ne "print unless /^\s*$MOD\s*\\\\?\s*\$/;" vendor/lge/g6-common/g6-common-vendor.mk; done
+
 
 
 source build/envsetup.sh
