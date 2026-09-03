e6ab6d3850ccd7c8e5f7287ea84dcdbee87a074b
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-09-03 13:07:32 +0800

    Update a18.sh

--- a18.sh
+++ a18.sh
@@ -26,31 +26,31 @@ export TARGET_ENABLE_BLUR=true
 #export WITH_ADB_INSECURE=true
 
 
-grep -q 'errno != EINVAL && errno != ENOSYS' bionic/libc/upstream-openbsd/android/include/arc4random.h && echo "already applied, skipping" || curl -sL https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/arc4random_wipeonfork1.patch | patch -p1
+@grep -q 'errno != EINVAL && errno != ENOSYS' bionic/libc/upstream-openbsd/android/include/arc4random.h && echo "already applied, skipping" || curl -sL https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/arc4random_wipeonfork1.patch | patch -p1
 
 #sed -i '$a -include vendor/lineage-priv/keys/keys.mk' device/lge/msm8996-common/msm8996.mk
 
 #perl -0777 -pi -e 's/^cc_prebuilt_library_shared \{\n\tname: "libwifi-hal-ctrl",.*?\n\}\n\n?//ms' vendor/lge/msm8996-common/Android.bp
 
-sed -i 's/libbinder-v32/libbinder/g; s/libprotobuf-cpp-lite-v29/libprotobuf-cpp-lite/g' vendor/lge/msm8996-common/Android.bp
+# sed -i 's/libbinder-v32/libbinder/g; s/libprotobuf-cpp-lite-v29/libprotobuf-cpp-lite/g' vendor/lge/msm8996-common/Android.bp
 
-sed -i '/name: "libkeystore_binder",/,/^}$/{
-  s/prefer: true,/prefer: true,\n\tcheck_elf_files: false,/
-}' vendor/lge/msm8996-common/Android.bp
-
-
-sed -i '/name: "libwvdrmengine",/,/^}$/{
-  s/prefer: true,/prefer: true,\n\tcheck_elf_files: false,/
-}' vendor/lge/msm8996-common/Android.bp
-
-sed -i '/name: "libwvhidl",/,/^}$/{
-  s/prefer: true,/prefer: true,\n\tcheck_elf_files: false,/
-}' vendor/lge/msm8996-common/Android.bp
+# sed -i '/name: "libkeystore_binder",/,/^}$/{
+#   s/prefer: true,/prefer: true,\n\tcheck_elf_files: false,/
+# }' vendor/lge/msm8996-common/Android.bp
 
 
-sed -i '/LOCAL_MODULE       := init.radio.sh/,/include \$(BUILD_PREBUILT)/{
-  s/LOCAL_VENDOR_MODULE    := true/LOCAL_VENDOR_MODULE    := true\nLOCAL_CHECK_ELF_FILES := false/
-}' device/lge/g6-common/rootdir/Android.mk
+# sed -i '/name: "libwvdrmengine",/,/^}$/{
+#   s/prefer: true,/prefer: true,\n\tcheck_elf_files: false,/
+# }' vendor/lge/msm8996-common/Android.bp
+
+# sed -i '/name: "libwvhidl",/,/^}$/{
+#   s/prefer: true,/prefer: true,\n\tcheck_elf_files: false,/
+# }' vendor/lge/msm8996-common/Android.bp
+
+
+# sed -i '/LOCAL_MODULE       := init.radio.sh/,/include \$(BUILD_PREBUILT)/{
+#   s/LOCAL_VENDOR_MODULE    := true/LOCAL_VENDOR_MODULE    := true\nLOCAL_CHECK_ELF_FILES := false/
+# }' device/lge/g6-common/rootdir/Android.mk
 
 sed -i '/# Add wlan to PRODUCT_SOONG_NAMESPACES/,/hardware\/qcom-caf\/wlan\/qcwcn/{
   /# Add wlan to PRODUCT_SOONG_NAMESPACES/i\
